190d3ac769b6c41b5d0d337b674ad8303d297a6c
Ted <ted_hodges@ymail.com>
2018-06-09 14:11:44 -0500

    added images and framework

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Navbar from "./components/Navbar";
+import Jumbotron from "./components/Jumbotron";
+import Main from "./components/Main";
+import Image from "./components/Image";
+import Images from "./components/Images.json"
+
+import archer from "./images/archer.jpg"
+import bear from "./images/bear.jpg"
+import bravo from "./images/bravo.jpg"
+import homer from "./images/homer.jpg"
+import peter from "./images/peter.jpg"
+import scooby from "./images/scooby.jpg"
+import simpsons from "./images/simpsons.jpg"
+import southpark from "./images/southpark.jpg"
+import spongebob from "./images/spongebob.jpg"
+import stewie from "./images/stewie.jpg"
+import stoner from "./images/stoner.jpg"
+import wiley from "./images/wiley.jpg"
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Navbar />
+        <Jumbotron />
+        <Main />
+            <Image  />
       </div>
     );
   }
---
+++ clicky-game/src/components/Jumbotron.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Jumbotron = () => (
+  <div className="jumbotron">
+    <h1 className="text-center">Click the Characters!</h1>
+    <h3 className="text-center">Click on an image to earn points, but don't click on any more than once!</h3>
+  </div>
+)
+export default Jumbotron;
