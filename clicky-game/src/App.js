d543f7eb176178f13748ee38a50cd0a76f610e73
Ted <ted_hodges@ymail.com>
2018-06-11 19:15:14 -0500

    now functioning

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -23,72 +23,73 @@ import './App.css';
 
 class App extends Component {
   state = {
-    Images
+    picked: [],
+    correct: 0,
+    topscore: 0,
+    message: 'Click an image!'
   };
 
 
-  
+  shuffleArray = (array) => {
+    let imgArray = Images;
+    for (let i = imgArray.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [imgArray[i], imgArray[j]] = [imgArray[j], imgArray[i]];
+    }
+    return imgArray
+  }
 
-  // shuffleArray = (array) => {
-  //   let imgArray = Image;
-  //   for (let i = imgArray.length - 1; i > 0; i--) {
-  //     let j = Math.floor(Math.random() * (i + 1));
-  //     [imgArray[i], imgArray[j]] = [imgArray[j], imgArray[i]];
-  //   }
-  //   return imgArray
-  // }
-
-  // pickImg = (name) => {
-  //   let picked = this.state.picked;
+  pickImg = (name) => {
+    let picked = this.state.picked;
     
-  //   if (picked.indexOf(name) === -1) {
-  //     this.setState({
-  //       picked: picked.concat(name),
-  //       correct: this.state.correct + 1,
-  //       topscore: this.state.correct + 1 > this.state.topscore ? this.state.correct + 1 : this.state.topscore,
-  //       message: "Correct!" 
-  //     })
-  //     this.shuffleArray();
-  //   }
-  //   else {
-  //     this.setState({
-  //       correct: 0,
-  //       picked: [],
-  //       message: "Wrong!"
-  //     })
-  //   }
-  // }
+    if (picked.indexOf(name) === -1) {
+      this.setState({
+        picked: picked.concat(name),
+        correct: this.state.correct + 1,
+        topscore: this.state.correct + 1 > this.state.topscore ? this.state.correct + 1 : this.state.topscore,
+        message: "Correct!" 
+      })
+      this.shuffleArray();
+    }
+    else {
+      this.setState({
+        correct: 0,
+        picked: [],
+        message: "Wrong!"
+      })
+    }
+  }
 
-  // imgSwitch = (name) => {
-  //   switch (name) {
-  //     case "archer":
-  //       return `${archer}`
-  //     case "bear":
-  //       return `${bear}`
-  //     case "bravo":
-  //       return `${bravo}`
-  //     case "homer":
-  //       return `${homer}`
-  //     case "peter":
-  //       return `${peter}`
-  //     case "scooby":
-  //       return `${scooby}`
-  //     case "simpsons":
-  //       return `${simpsons}`
-  //     case "southpark":
-  //       return `${southpark}`
-  //     case "spongebob":
-  //       return `${spongebob}`
-  //     case "stewie":
-  //       return `${stewie}`
-  //     case "stoner":
-  //       return `${stoner}`
-  //     case "wiley":
-  //       return `${wiley}`
-  //     default:
-  //       return `${archer}`
-  //   }
-  // }
+  imgSwitch = (name) => {
+    switch (name) {
+      case "archer":
+        return `${archer}`
+      case "bear":
+        return `${bear}`
+      case "bravo":
+        return `${bravo}`
+      case "homer":
+        return `${homer}`
+      case "peter":
+        return `${peter}`
+      case "scooby":
+        return `${scooby}`
+      case "simpsons":
+        return `${simpsons}`
+      case "southpark":
+        return `${southpark}`
+      case "spongebob":
+        return `${spongebob}`
+      case "stewie":
+        return `${stewie}`
+      case "stoner":
+        return `${stoner}`
+      case "wiley":
+        return `${wiley}`
+      default:
+        return `${archer}`
+    }
+  }
 
   render() {
     return (
@@ -96,14 +97,10 @@ class App extends Component {
         <Navbar correct={this.state.correct} topscore={this.state.topscore} message={this.state.message}/>
         <Jumbotron />
         <Main>
-        {this.state.Images.map(friend => (
-          <Image
-            name={friend.name}
-            url={friend.url}
-    
-          />
-        ))}
-      </Main>
+          {this.shuffleArray(Images).map(image => (
+            <Image src={this.imgSwitch(image.name)} name={image.name} key={image.name} pickImg={this.pickImg}  />
+          ))}
+        </Main>
       </div>
     );
   }
